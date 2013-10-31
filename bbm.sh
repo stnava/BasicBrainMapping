6b15e2ff4426d3d8d4bb213742bda1cc4b0805ec
brian avants <stnava@gmail.com>
2013-10-31 16:00:53 -0400

    ENH: lesion example

--- bbm.sh
+++ bbm.sh
@@ -44,42 +44,27 @@ $reg -d $dim -r [ $imgs ,1]  \
 ${AP}antsApplyTransforms -d $dim -i $m -r $f -n linear -t ${nm}1Warp.nii.gz -t ${nm}0GenericAffine.mat -o ${nm}_warped.nii.gz --float 1 
 fi  
 
-echo Do Lesion Study 
+echo Lesion study uses the affine mapping from the previous result.
+echo One could also "revise" the template mask by the lesion mask but 
+echo here we assume the affine map is not corrupted by the lesion. 
+echo We do, however, mask the deformation estimation with a lesion mask.
+origmat=${nm}0GenericAffine.mat
 nm=BBM_Lesion
 SmoothImage 3 data/lesion.nii.gz 2 data/neg_lesion.nii.gz
 ImageMath 3  data/neg_lesion.nii.gz CorruptImage  data/neg_lesion.nii.gz 
-MultiplyImages 3 data/neg_lesion.nii.gz $m data/T1_lesioned.nii.gz 
-m=data/T1_lesioned.nii.gz
 ImageMath 3 data/neg_lesion.nii.gz Neg data/neg_lesion.nii.gz 
-CopyImageHeaderInformation $m data/neg_lesion.nii.gz data/neg_lesion.nii.gz 1 1 1 
+MultiplyImages 3 data/neg_lesion.nii.gz $2 data/T1_lesioned.nii.gz 
+m=data/T1_lesioned.nii.gz
 imgs=" $m, $f "
 myit=1000
-$reg -d $dim -r [ $imgs ,1]  \
-                        -m mattes[  $imgs , 1 , 32, regular, 0.05 ] \
-                         -t translation[ 0.1 ] \
-                         -c [${myit},1.e-8,20]  \
-                        -s 4vox  \
-                        -f 6 -l 1 \
-                        -m mattes[  $imgs , 1 , 32, regular, 0.1 ] \
-                         -t rigid[ 0.1 ] \
-                         -c [${myit}x${myit},1.e-8,20]  \
-                        -s 4x2vox  \
-                        -f 4x2 -l 1 \
-                        -m mattes[  $imgs , 1 , 32, regular, 0.1 ] \
-                         -t affine[ 0.1 ] \
-                         -c [${myit}x100x10,1.e-8,20]  \
-                        -s 4x2x1vox  \
-                        -f 3x2x1 -l 1 \
-                        -m mattes[  $imgs , 1 , 32 ] \
-                         -t SyN[ .20, 3, 0 ] \
-                         -c [ 30x20x0 ]  \
-                        -s 1x0.5x0vox  \
-                        -f 4x2x1 -l 1 -u 1 -z 1 -x data/neg_lesion.nii.gz   --float 1 \
-                       -o [${nm},${nm}_diff.nii.gz,${nm}_inv.nii.gz]
-
-${AP}antsApplyTransforms -d $dim -i $f -r $m -n linear -t ${nm}1Warp.nii.gz -t ${nm}0GenericAffine.mat -o ${nm}_warped.nii.gz --float 1 
-
-ExtractSliceFromImage 3 data/T1_lesioned.nii.gzz temp.nii.gz 1 120
+$reg -d $dim -r [${origmat},1] \
+    -m mattes[  $imgs , 1 , 32 ] \
+    -t SyN[ .20, 3, 0.1 ] \
+    -c [ 30x20x0 ]  \
+    -s 1x0.5x0vox  \
+    -f 4x2x1 -l 1 -u 1 -z 1 -x data/neg_lesion.nii.gz   \
+    -o [${nm},${nm}_diff.nii.gz,${nm}_inv.nii.gz]
+ExtractSliceFromImage 3 data/T1_lesioned.nii.gz temp.nii.gz 1 120
 ConvertImagePixelType temp.nii.gz T1_lesioned.jpg 1
-ExtractSliceFromImage 3 BBM_warped.nii.gz temp.nii.gz 1 120
+ExtractSliceFromImage 3 ${nm}_diff.nii.gz temp.nii.gz 1 120
 ConvertImagePixelType temp.nii.gz Template2T1_lesioned.jpg 1
