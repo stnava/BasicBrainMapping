e15769334e430c65a1fdcb07f88d2926f5a2ba4f
stnava <stnava@gmail.com>
2016-08-17 08:47:21 -0400

    PERF: increase size for purpose of drama

--- bbm2.R
+++ bbm2.R
@@ -1,13 +1,17 @@
 #!/usr/bin/env Rscript
 library( ANTsR )
-negLesion = -antsImageRead( "data/LesionMap.nii.gz")
+lesion = antsImageRead( "data/LesionMap.nii.gz") %>%
+  iMath( "MD", 15 ) # make it larger for more dramatic effect
+negLesion = -lesion
 f = antsImageRead( "data/T1_with_lesion.nii.gz" )
 m = antsImageRead( "data/IXI/T_template2.nii.gz" )
 areg = antsRegistration( f, m, mask=negLesion, typeofTransform="SyN",
-  regIterations = c(40,20,0,0), verbose=T ) # change iterations
+  regIterations = c(100,100,40,0), verbose=T ) # change iterations
 jac = createJacobianDeterminantImage( f, areg$fwdtransforms[1] )
-grid = createWarpedGrid( f, gridStep = 10, gridWidth = 2,
+grid = createWarpedGrid( m, gridStep = 10, gridWidth = 2,
   gridDirections = c(T,F,T), fixedReferenceImage = f,
   transform = areg$fwdtransforms )
+antsImageWrite( jac , '/tmp/jac.nii.gz' )
+antsImageWrite( grid , '/tmp/grid.nii.gz' )
 # CreateJacobianDeterminantImage 3 ${nm}_diff1Warp.nii.gz jacobian.nii.gz 1
 # CreateWarpedGridImage 3 ${nm}_diff1Warp.nii.gz grid.nii.gz 1x0x1 10x10x10 3x3x3
